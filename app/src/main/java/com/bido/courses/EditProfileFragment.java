ca4e4787308a52e4a0145653d32b443076f1d14c
abdelrhman.m <Abdelrhman683>
2021-10-28 01:08:50 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/bido/courses/SignUpActivity.java

--- app/src/main/java/com/bido/courses/EditProfileFragment.java
+++ app/src/main/java/com/bido/courses/EditProfileFragment.java
@@ -2,63 +2,135 @@ package com.bido.courses;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EditProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+
+import com.bido.courses.firebase.UserData;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+import java.util.HashMap;
+
+
+
 public class EditProfileFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public EditProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EditProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EditProfileFragment newInstance(String param1, String param2) {
-        EditProfileFragment fragment = new EditProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    EditText profile_frist_name,profile_last_name,profile_email;
+    Button profile_bt_update;
+    String uid;
+    FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+
+        View view=inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        profile_frist_name=view.findViewById(R.id.profile_frist_name);
+        profile_last_name=view.findViewById(R.id.profile_last_name);
+        profile_email=view.findViewById(R.id.profile_email);
+        profile_bt_update=view.findViewById(R.id.button16);
+        uid = firebaseAuth.getCurrentUser().getUid();
+        getuserdata();
+        profile_bt_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                updateUsername();
+            }
+        });
+        return  view;
     }
+
+
+    private void updateUsername() {
+        String fname=profile_frist_name.getText().toString().trim();
+        String lname=profile_last_name.getText().toString().trim();
+
+        if (fname.isEmpty()||lname.isEmpty()){
+            Toast.makeText(getActivity(), "Please write your name", Toast.LENGTH_SHORT).show();
+            profile_frist_name.setError("Frist Name required");
+            profile_last_name.setError("last Name required");
+
+            return;
+
+        }
+        HashMap<String, Object> firstnameMap = new HashMap<>();
+        firstnameMap.put("fname", fname);
+        firestore.collection("Account").document(uid)
+                .update(firstnameMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getActivity(), "profile is update", Toast.LENGTH_SHORT).show();
+                } else {
+                    String errorMassae = task.getException().getLocalizedMessage();
+                    Toast.makeText(getActivity(), errorMassae, Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+        HashMap<String, Object> lastnameMap = new HashMap<>();
+        lastnameMap.put("lname", lname);
+        firestore.collection("Account").document(uid)
+                .update(lastnameMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+
+
+                } else {
+                    String errorMassae = task.getException().getLocalizedMessage();
+                    Toast.makeText(getActivity(), errorMassae, Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+
+
+
+    }
+
+    private void updateUI(UserData userData) {
+        profile_frist_name.setText(userData.getFname());
+        profile_last_name.setText(userData.getLname());
+        profile_email.setText(userData.getEmail());
+
+
+    }
+    private void getuserdata() {
+
+        firestore.collection("Account")
+                .document(uid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+
+                    UserData userData = task.getResult().toObject(UserData.class);
+                    updateUI(userData);
+
+                } else {
+                    String errorMassae = task.getException().getLocalizedMessage();
+                    Toast.makeText(getActivity(), errorMassae, Toast.LENGTH_SHORT).show();
+
+
+                }
+
+            }
+        });
+    }
+
 }
